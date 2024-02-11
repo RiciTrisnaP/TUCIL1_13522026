ad3d4fa21db78feed4fa0ded4d7057c667ade603
RiciTrisnaP <ricitrisnap@gmail.com>
2024-02-12 04:26:46 +0700

    remove debugging copy

--- src/solution_finder.py
+++ src/solution_finder.py
@@ -5,6 +5,7 @@ import numpy as np
 matrix,buffer,sequences = read_txt("test.txt")
 matrix_cp = matrix.copy()
 current_optimum = np.full([buffer.size], '??')
+step_optimum = []
 list_step = np.full([buffer.size,2],-1)
 start_position = np.array([0,0])
 direction = "horizontal"
@@ -13,27 +14,16 @@ step = 0
 
 def solution_finder(matrix, buffer, sequences, direction, start_position, list_step, step):
     global current_optimum 
+    global step_optimum
     global max_weight
     if step != buffer.size:
         if direction == "horizontal":
             direction = "vertical"
             step = step + 1
             for i in range(matrix.width):
-                print("")
-                print("-------------------------------------")
-                matrix.print()
-                buffer.print()
-                sequences.print()
-                print(direction)
-                print(start_position)
-                print(list_step)
-                print(step)
-                print("-------------------------------------")
                 new_position = np.copy(start_position)
                 new_position[1] = i
-                print(new_position)
                 if matrix.get_cell(new_position[0],new_position[1]) == '##':
-                    print("masuk")
                     continue
                 list_step[step-1] = np.array([new_position[0],new_position[1]])
                 buffer.set_element(step-1,matrix.get_cell(new_position[0],new_position[1]))
@@ -42,28 +32,13 @@ def solution_finder(matrix, buffer, sequences, direction, start_position, list_s
                 matrix.set_cell(list_step[step-1][0],list_step[step-1][1],buffer.get_element(step-1))
                 buffer.set_element(step-1,'??')
                 list_step[step-1] = np.array([-1,-1])
-                print('FJDKSLJFLDS')
-                print(new_position)
-                matrix.print()
         else:
             direction = "horizontal"
             step = step + 1
             for i in range(matrix.height):
-                print("")
-                print("-------------------------------------")
-                matrix.print()
-                buffer.print()
-                sequences.print()
-                print(direction)
-                print(start_position)
-                print(list_step)
-                print(step)
-                print("-------------------------------------")
                 new_position = np.copy(start_position)
                 new_position[0] = i
-                print(new_position)
                 if matrix.get_cell(new_position[0],new_position[1]) == '##':  
-                    print("masuk")
                     continue
                 list_step[step-1] = np.array([new_position[0],new_position[1]])
                 buffer.set_element(step-1,matrix.get_cell(new_position[0],new_position[1]))
@@ -72,25 +47,16 @@ def solution_finder(matrix, buffer, sequences, direction, start_position, list_s
                 matrix.set_cell(list_step[step-1][0],list_step[step-1][1],buffer.get_element(step-1))
                 buffer.set_element(step-1,'??')
                 list_step[step-1] = np.array([-1,-1])
-                print('FJDKSLJFLDS')
-                print(new_position)
-                matrix.print()
     else:
-        print("")
-        print("--------------WEIGHT---------------")
-        matrix.print()
-        buffer.print()
-        sequences.print()
-        print(direction)
-        print(start_position)
-        print(list_step)
-        print(step)
-        print("-------------------------------------")
-        lol = sequences.match_all(buffer)
-        print(lol)
-        if lol > max_weight:
-            max_weight = lol
+        weight = sequences.match_all(buffer)
+        if weight > max_weight:
+            max_weight = weight
+            step_optimum = list_step.tolist()
             current_optimum = np.copy(buffer.list)
-            print(current_optimum)
 
 solution_finder(matrix,buffer,sequences,direction,start_position,list_step,step)
+print(max_weight)
+for x in current_optimum:
+    print(f"{x} ",end="")
+for x in step_optimum:
+    print(f"{x[0]} {x[1]}")
